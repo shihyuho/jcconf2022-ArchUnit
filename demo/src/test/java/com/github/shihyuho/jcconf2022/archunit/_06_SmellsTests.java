30bf2a6596351b3e14cc63bd746ca7581b31d0f1
Shihyu Ho <methodho@gmail.com>
2022-10-06 18:29:48 +0800

    feat: update

--- demo/src/test/java/com/github/shihyuho/jcconf2022/archunit/_06_SmellsTests.java
+++ demo/src/test/java/com/github/shihyuho/jcconf2022/archunit/_06_SmellsTests.java
@@ -1,9 +1,8 @@
 package com.github.shihyuho.jcconf2022.archunit;
 
 
-import com.tngtech.archunit.core.domain.JavaAccess;
+import com.tngtech.archunit.core.domain.JavaCall;
 import com.tngtech.archunit.core.domain.JavaClass;
-import com.tngtech.archunit.core.domain.JavaConstructor;
 import com.tngtech.archunit.core.domain.JavaMethod;
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeJars;
 import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
@@ -13,7 +12,6 @@ import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.SimpleConditionEvent;
-import com.tngtech.archunit.lang.conditions.ArchConditions;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 
 @AnalyzeClasses(packagesOf = MyApplication.class, importOptions = {
@@ -22,27 +20,24 @@ import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 })
 class _06_SmellsTests {
 
-  @ArchTest
-  static final ArchRule methods_with_long_parameter_list =
-    ArchRuleDefinition.methods().should(haveParametersAtMost3(3));
 
-  private static ArchCondition<JavaMethod> haveParametersAtMost3(final int maxParameters) {
-    return new ArchCondition<>("have parameters at most " + maxParameters) {
+  @ArchTest
+  static ArchRule long_parameters_list = ArchRuleDefinition.methods()
+    .should(new ArchCondition<>("have parameters at most 3") {
       @Override
       public void check(JavaMethod item, ConditionEvents events) {
-        if (item.getParameters().size() > maxParameters) {
+        if (item.getParameters().size() > 3) {
           events.add(
             SimpleConditionEvent.violated(item,
               "%s have %s parameters".formatted(item.getDescription(), item.getParameters().size()))
           );
         }
       }
-    };
-  }
+    });
 
   @ArchTest
-  static final ArchRule classes_too_large =
-    ArchRuleDefinition.classes().should(new ArchCondition<>("contain less than 30 fields") {
+  static ArchRule large_class = ArchRuleDefinition.classes()
+    .should(new ArchCondition<>("contains less than 30 fields") {
       @Override
       public void check(JavaClass item, ConditionEvents events) {
         if (item.getFields().size() > 30) {
@@ -54,23 +49,28 @@ class _06_SmellsTests {
       }
     });
 
-
   @ArchTest
-  static final ArchRule long_method =
-    ArchRuleDefinition.methods()
-      .should(new ArchCondition<>("not have more than 30 code lines") {
-        @Override
-        public void check(JavaMethod item, ConditionEvents events) {
-          var firstLine = item.getSourceCodeLocation().getLineNumber();
-          var lastLine = item.getCallsFromSelf().stream().mapToInt(JavaAccess::getLineNumber)
-            .max().orElse(firstLine);
-          if (lastLine - firstLine > 30) {
-            events.add(
-              SimpleConditionEvent.violated(item,
-                "%s have %s code lines".formatted(item.getDescription(), lastLine - firstLine))
-            );
-          }
+  static ArchRule long_method = ArchRuleDefinition.methods()
+    .should(notHaveCodeLinesMoreThan(30));
+
+  private static ArchCondition<JavaMethod> notHaveCodeLinesMoreThan(final int maxLines) {
+    return new ArchCondition<>("not have no more than %s code lines".formatted(maxLines)) {
+      @Override
+      public void check(JavaMethod item, ConditionEvents events) {
+        var first = item.getSourceCodeLocation().getLineNumber();
+        var last = item.getCallsFromSelf().stream().mapToInt(JavaCall::getLineNumber).max()
+          .orElse(first);
+        var lines = last - first + 1;
+        if (lines > maxLines) {
+          events.add(
+            SimpleConditionEvent.violated(
+              item,
+              "%s have %s code lines".formatted(item.getDescription(), lines)
+            )
+          );
         }
-      });
+      }
+    };
+  }
 
 }
